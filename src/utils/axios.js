6bc11f3179e217f2d63083e96da9e6fab0b5cff2
ArtemMerkushyn <merkushyndev13@gmail.com>
2023-12-19 18:23:07 +0200

    commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    //baseURL: 'http://localhost:8080/api'
-    baseURL: 'https://assortment-sounds-api.vercel.app/api'
+    baseURL: 'http://localhost:8080/api'
+    //baseURL: 'https://assortment-sounds-api.vercel.app/api'
 });
 
 export default instance;
