342c71af22dd732a6929ce2d293faac4bca3f9cc
ArtemMerkushyn <merkushyndev13@gmail.com>
2023-12-19 18:35:11 +0200

    axios

--- src/pages/VinilsPage.jsx
+++ src/pages/VinilsPage.jsx
@@ -1,4 +1,4 @@
-import axios from '../utils/axios.js';
+//import axios from '../utils/axios.js';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
